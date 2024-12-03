9dddaff7465f68a629309dc0fedf0ffd48855120
Angelo Rafael <113860124+RafaSicron@users.noreply.github.com>
2024-12-03 14:04:24 -0300

    Sidebar finalizada

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from '@phosphor-icons/react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,11 +9,16 @@ export function Sidebar() {
              src="https://images.unsplash.com/photo-1565843708714-52ecf69ab81f?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/RafaSicron.png"/>
                 <strong>Angelo Rafael</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+               
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
